f1263ac8e18907dd004a9fcacc6d32dffb747ed7
ThanhdndHE186428 <ThanhdndHE186428@fpt.edu.vn>
2025-10-04 02:47:59 +0700

    Update v3

--- medconnect/client/src/routes/publicRoutes.jsx
+++ medconnect/client/src/routes/publicRoutes.jsx
@@ -1,11 +1,12 @@
 import DefaultLayout from "../layouts/DefaultLayout/DefaultLayout";
 import AuthLayout from "../layouts/AuthLayout/AuthLayout";
-// The homepage component is located at pages/Home/Homepage.jsx
-import Home from "../pages/Home/Homepage";
+// The homepage component is located at pages/Home/Homepage/Homepage.jsx
+import Home from "../pages/Home/Homepage/Homepage";
 import About from "../pages/About/About";
 import Products from "../pages/Products/Products";
 import ProductDetail from "../pages/Products/ProductDetail";
-import HomeVisit from "../pages/HomeVisit/HomeVisit";
+import HomeVisit from "../pages/Home/HomeVisit/HomeVisit";
+import HospitalVisit from "../pages/Home/HospitalVisit/HospitalVisit";
 import Login from "../pages/Auth/Login";
 import { Route } from "react-router-dom";
 import GuestMiddleware from "../middlewares/GuestMiddleware";
@@ -15,6 +16,7 @@ export const publicRoutes = (
       <Route path="/" element={<Home />} />
       <Route path="/gioi-thieu" element={<About />} />
       <Route path="/kham-tai-nha" element={<HomeVisit />} />
+      <Route path="/kham-tai-vien" element={<HospitalVisit />} />
       <Route path="/san-pham">
         <Route path="" element={<Products />} />
         <Route path=":id" element={<ProductDetail />} />
